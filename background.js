c69a3ca474fdb5b4a5155124dac864b593eb8093
hemanthgowda <iamhemanth01@gmail.com>
2025-01-26 19:22:39 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("Extension installed!");
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  console.log('Received message:', message);
+  if (message.type === 'getMeaning') {
+    // Your logic for handling the message
+    sendResponse({ result: 'Meaning of word' });
+  }
 });
-
-// Add other event listeners or logic you need
