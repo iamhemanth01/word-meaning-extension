9962638b9a277e779ba80a52b7ab2c87f4ab9281
hemanthgowda <iamhemanth01@gmail.com>
2025-01-26 19:27:32 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,6 +1,8 @@
 document.addEventListener("mouseup", () => {
   const selectedText = window.getSelection().toString().trim();
   if (selectedText) {
-    chrome.runtime.sendMessage({ word: selectedText });
+    chrome.runtime.sendMessage({ word: selectedText }, (response) => {
+      console.log('Response from background:', response);
+    });
   }
 });
